71c6546b59e362c53e7d2f4af2c884d94b41688a
Estevan Gonzalez <egonza013@gmail.com>
2018-02-10 14:28:26 -0600

    -Changed array of initial locations to allow data to pass through for loop easier.
    -Added for loop to display locations and set bounds based on marker
    location.

--- js/app.js
+++ js/app.js
@@ -3,25 +3,49 @@ var markers = [];
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.719526, lng: -74.0089934},
+    center: {lat: 29.9287953, lng: -95.4114391},
     zoom: 13
   });
-  var tribeca = {lat: 40.719526, lng: -74.0089934};
+  // create instance of infoWindow
+  var largeInfoWindow = new google.maps.InfoWindow();
+  // used for the view to set boundaries
+  var bounds = new google.maps.LatLngBounds();
+  // for loop to iterate through initial locatiosn and create markers
+  for (var i = 0; i < locations.length; i++) {
+    var position = locations[i].location;
+    var title = locations[i].title;
+    // Create market for each location and push into markers array
+    var marker = new google.maps.Marker({
+      map: map,
+      position: position,
+      title: title,
+      animation: google.maps.Animation.DROP,
+      id: i
+    });
+    // push the marker to our array 'markers'
+    markers.push(marker);
+    // Extend bounds of the map for each marker
+    bounds.extend(marker.position);
+    // Create onclick event listener to open infowindow at each marker.
+    marker.addListener('click', function() {
+      populateInfoWindow(this, largeInfoWindow);
+    });
 
-  var marker = new google.maps.Marker({
-    position: tribeca,
-    map: map,
-    title: 'First Marker!'
-  });
-  // Creates an instance of GoogleMaps InfoWindow class with the attribute of content
-  var infoWindow = new google.maps.InfoWindow({
-    content: 'Do you ever feel like an infoWindow' + ' ready to start again?'
-  });
-  // The InfoWindow does not automatically open. This creates a click listener to open when marker is clicked
-  marker.addListener('click', function() {
-    infoWindow.open(map, marker);
-  });
-
+  }
+  map.fitBounds(bounds);
+  // function populates the infoWindow when the marker is clicked.
+  function populateInfoWindow(marker, infoWindow) {
+    // check to see if infowindow is already open for this marker
+    if (infoWindow.marker != marker) {
+      infoWindow.marker = marker;
+      infoWindow.setContent('<div>' + marker.title + '</div>');
+      infoWindow.open(map, marker);
+      // Make sure the marker property is cleared if the infowindow is closed.
+      infoWindow.addListener('closeclick', function() {
+        infoWindow.setMarker(null);
+      });
+    }
+  }
 }
 
 var Location = function (data) {
@@ -45,7 +69,6 @@ var ViewModel = function() {
 
     self.filter = ko.observable("");
     self.locationList = ko.observableArray([]);
-
 
     locations.forEach(function(locationItem){
       self.locationList.push( new Location(locationItem) );
--- js/locations.js
+++ js/locations.js
@@ -1,37 +1,30 @@
 var locations = [
     {
         title: 'Mueschke Garage',
-        lat: 30.0180852,
-        lng:  -95.7249338,
+        location: {lat: 30.0180852, lng:  -95.7249338}
     },
     {
         title: 'Reed Rd Garage',
-        lat: 29.6565847,
-        lng: -95.3872281,
+        location: {lat: 29.6565847, lng:  -95.3872281}
     },
     {
         title: 'Airline Garage',
-        lat: 29.9287953,
-        lng: -95.4114391,
+        location: {lat: 29.9287953, lng:  -95.4114391}
     },
     {
         title: 'Kempwood Garage',
-        lat: 29.8221091,
-        lng: -95.568593,
+        location: {lat: 29.8221091, lng:  -95.568593}
     },
     {
         title: 'Wallisville Garage',
-        lat: 29.7914907,
-        lng: -95.2677757,
+        location: {lat: 29.7914907, lng:  -95.2677757}
     },
     {
         title: 'Charles Garage',
-        lat: 29.8831646,
-        lng: -95.5834527,
+        location: {lat: 29.8831646, lng:  -95.5834527}
     },
     {
         title: 'Spring Cypress Garage',
-        lat: 30.0458043,
-        lng: -95.5438207,
+        location: {lat: 30.0458043, lng:  -95.5438207}
     }
 ]
